2d69c7846b42824f680aa7c1e2f405fb3b7cbc97
vampirerachel <vampirerachel@gmail.com>
2023-04-22 14:13:30 -0400

    draft 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,65 @@
 import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const formEl = document.querySelector("#search-form");
 const input = document.querySelector('[name ="searchQuery"]')
 const gallery = document.querySelector('.gallery')
 const loadButton = document.querySelector('.load-more')
+const searchButton = document.querySelector("button")
 loadButton.style.display = "none";
 let pageNumber = 1;
 
 
 
 async function getData(e) {
+    
+    gallery.innerHTML = "";
     e.preventDefault()
     loadButton.style.display = "block"
     const userSearch = input.value
     const res = await axios.get(
     `https://pixabay.com/api/?key=35513783-cdd32f526a75b86a8cfb6c8f5&q=${userSearch}&image_type=photos&orientation=horizontal&safesearch=true&per_page=40&page=${pageNumber}`);
-    
     res.data.hits.forEach(item => {
     const div = document.createElement('div');
-    div.setAttribute('class', 'photo-card');
+    div.setAttribute('class', 'photo-container');
     div.innerHTML =`
     <div class="photo-card">
-    <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+        <a href ="${item.webformatURL}" alt="${item.tags}">
+    <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></a>
     <div class="info">
     <p class="info-item">
-    <b>Likes : ${item.likes}</b>
+    <b>Likes: ${item.likes}</b>
     </p>
     <p class="info-item">
     <b>Views: ${item.views}</b>
     </p>
     <p class="info-item">
-    <b>Comments ${item.comments}</b>
+    <b>Comments: ${item.comments}</b>
     </p>
     <p class="info-item">
-    <b>Downloads${item.downloads}</b>
+    <b>Downloads: ${item.downloads}</b>
     </p>
     </div>
     </div>`;
         gallery.style.display = "flex";
-        gallery.style.justifyContent = "center";
-
-gallery.append(div);
+        gallery.style.justifyContent = " space-between";
+        gallery.style.flexWrap = "wrap";
+        gallery.style.margin = "0 auto";
+        gallery.style.width = "1200px"
+        gallery.append(div);
     })
-    Notify.info("Hooray we found ${res.data.hits} matches")
-    if (res.data.hits.length === 0) {
+    
+    let hits = res.data.total
+
+    if (hits > 0) {
+        Notify.info("Hooray we found " + hits + " matches")
+    }
+    if (hits === 0) {
         Notify.info("Sorry, there are no images matching your search query. Please try again.")
 }
+
+
 }
 
 
@@ -53,10 +67,8 @@ function handleSubmit(e) {
     /* when click on load more then increase page number and call get data*/
     pageNumber++;
     getData(e)
-
 }
 
 loadButton.addEventListener("click", handleSubmit)
 formEl.addEventListener("submit", getData);
 
-/*TODO Styles, and alerts(if (res.data.totalHits)), simplelightbox */
